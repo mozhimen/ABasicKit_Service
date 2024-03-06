f8bca4c112f250fe8f57d7ebd150b05971a2a305
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-03-06 20:14:32 +0800

    0306

---
+++ basick_service/src/main/java/com/mozhimen/basick/service/bases/BaseService.kt
@@ -0,0 +1,61 @@
+package com.mozhimen.basick.service.bases
+
+import android.app.Service
+import android.os.RemoteCallbackList
+import android.os.RemoteException
+import com.mozhimen.basick.service.commons.IBaseServiceConnListener
+import com.mozhimen.basick.service.commons.IBaseServiceResListener
+import com.mozhimen.basick.elemk.commons.IA_Listener
+import com.mozhimen.basick.utilk.android.util.et
+import com.mozhimen.basick.utilk.commons.IUtilK
+
+/**
+ * @ClassName BaseService
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2023/6/10 19:03
+ * @Version 1.0
+ */
+abstract class BaseService : Service(), IA_Listener<Any>, IUtilK {
+    protected val serviceResListeners = RemoteCallbackList<IBaseServiceResListener>()
+    protected open val binder: IBaseServiceConnListener.Stub = BaseServiceBinder(serviceResListeners)
+
+    override fun invoke(obj: Any) {
+        val listenerSize = serviceResListeners.beginBroadcast()
+        try {
+            for (i in 0 until listenerSize) {
+                when (obj) {
+                    is Int -> {
+                        serviceResListeners.getBroadcastItem(i).onResInt(obj)
+                    }
+
+                    is Long -> {
+                        serviceResListeners.getBroadcastItem(i).onResLong(obj)
+                    }
+
+                    is Boolean -> {
+                        serviceResListeners.getBroadcastItem(i).onResBoolean(obj)
+                    }
+
+                    is Float -> {
+                        serviceResListeners.getBroadcastItem(i).onResFloat(obj)
+                    }
+
+                    is Double -> {
+                        serviceResListeners.getBroadcastItem(i).onResDouble(obj)
+                    }
+
+                    is String -> {
+                        serviceResListeners.getBroadcastItem(i).onResString(obj)
+                    }
+
+                    else -> {}
+                }
+            }
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+            e.message?.et(TAG)
+        }
+        serviceResListeners.finishBroadcast()
+    }
+}
